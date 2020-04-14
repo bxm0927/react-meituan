b7c980b2c5bce68d4d26a6f0f370e6377301d74a
bxm0927 <80583600@qq.com>
2020-04-14 16:04:48 +0800

    eslint prettier

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { render } from "react-dom";
-import { Provider } from "react-redux";
-import App from "./App";
-import store from "@/store";
-import "@/assets/stylesheets/reset.scss";
-import "@/assets/stylesheets/common.scss";
-import "@/assets/fonts/iconfont.css";
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './App'
+import store from '@/store'
+import '@/assets/stylesheets/reset.scss'
+import '@/assets/stylesheets/common.scss'
+import '@/assets/fonts/iconfont.css'
 
 render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
-);
+  document.getElementById('root'),
+)
