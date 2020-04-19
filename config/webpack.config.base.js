227153b79f91f9a801bd29ac96eff243e76f120d
bxm0927 <80583600@qq.com>
2020-04-19 14:39:57 +0800

    publicPath

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -2,7 +2,7 @@
  * @Author: xiaoming.bai
  * @Date: 2020-04-05 17:55:35
  * @Last Modified by: xiaoming.bai
- * @Last Modified time: 2020-04-19 14:32:41
+ * @Last Modified time: 2020-04-19 14:38:26
  */
 
 const fs = require('fs')
@@ -69,7 +69,7 @@ module.exports = {
     path: DIST_DIR,
     chunkFilename: '[name].bundle.js',
     filename: DEV_MODE ? 'js/[name].js' : 'js/[name].[hash:8].js',
-    publicPath: 'https://bxm0927.github.io/react-meituan/dist/',
+    publicPath: DEV_MODE ? '/' : 'https://bxm0927.github.io/react-meituan/',
   },
   resolve: {
     extensions: ['.js', '.json', '.jsx'],
